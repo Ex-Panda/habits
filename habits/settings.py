4f94a4a650919b2e226316b6e74bafff33002ae1
panda <frausonya131@gmail.com>
2024-01-25 19:03:20 +0300

    fix settings

--- habits/settings.py
+++ habits/settings.py
@@ -27,9 +27,9 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('URL').split(',')
 
 
 # Application definition
@@ -88,9 +88,10 @@ WSGI_APPLICATION = 'habits.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'habits',
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD')
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db'
     }
 }
 
@@ -144,15 +145,15 @@ REST_FRAMEWORK = {
 
 token_tg = os.getenv('token_tg')
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_TIMEZONE = "Europe/Moscow"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
 CELERY_BEAT_SCHEDULE = {
     'datetime_reminder': {
-        'task': 'main.service_func.datetime_reminder',
+        'task': 'main.tasks.datetime_reminder',
         'schedule': crontab(minute='*'),
     },
 }
